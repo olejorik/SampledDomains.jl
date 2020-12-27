3f1e3a2892e8959864112985f71d00615082c302
Oleg Soloviev <oleg.soloviev@gmail.com>
2020-12-27 16:20:47 +0100

    fist approach to SampledDomain

--- src/SampledDomains.jl
+++ src/SampledDomains.jl
@@ -1,17 +1,30 @@
+"""
+The goal of the module is to attach physical coordinaes to array elements, so `a[i,j]` could be interpreted as ``f(x_i, y_j)`` for some function ``f``.
+"""
 module SampledDomains
 
+abstract type AbstractDomain end
+
 import Base.:*
+import Base.size
 
 export CartesianDomain2D, make_centered_domain2D, dualRange, dualDomain
 
 # TODO rewrite as parametric type
-struct CartesianDomain2D
+struct CartesianDomain2D<:AbstractDomain
     xrange::AbstractRange
     yrange::AbstractRange
 end
 
+getranges(dom::AbstractDomain) = [getfield(dom,n) for n in fieldnames(typeof(dom))]
+
+size(dom::AbstractDomain) = tuple(length.(reverse(getranges(dom)))...) # we need reverse to follow column major rule
+# size(dom::AbstractDomain, d) = length(getfield(dom,d))
+size(dom::AbstractDomain, d) = size(dom)[d]
+
 *(dom::CartesianDomain2D, s::Real) = CartesianDomain2D(dom.xrange * s, dom.yrange*s)
 *( s::Real, dom::CartesianDomain2D) = CartesianDomain2D(dom.xrange * s, dom.yrange*s)
+
 
 function make_centered_domain2D(xlength, ylength, pixelsize)
     xrange = ((1:xlength) .- (1 + xlength) / 2) .* pixelsize
@@ -38,5 +51,19 @@ function dualDomain(dom::CartesianDomain2D, q::Int = 1)
     kyrange = dualRange(dom.yrange, q)
     return CartesianDomain2D(kxrange,kyrange)
 end
+
+"""
+    SampledDomain(vals::Array, dom::AbstractDomain) 
+    SampledDomain(f::Function, dom::AbstractDomain)
+
+Contains two fileds: `vals` and `dom` representing sampled values of function `f` on `dom`ain.
+"""
+struct SampledDomain
+    vals::Array
+    dom::AbstractDomain
+    SampledDomain(vals::Array, dom::AbstractDomain) = size(vals) != size(dom) ? ErrorException("Different size of the domain and values array") : new(vals, dom)
+end
+
+SampledDomain(f::Function, dom::AbstractDomain) = SampledDomain(map(x->f(x...), collect(Iterators.product(reverse(getranges(dom))...))), dom)
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,14 @@ using SampledDomains
 using Test
 
 @testset "SampledDomains.jl" begin
-    # Write your tests here.
+    a= SampledDomains.CartesianDomain2D(1:3, -5:.5:-3.5)
+    f(x,y) = x*y
+    b = SampledDomains.SampledDomain(f, a)
+    @test b.vals ==[
+        -5.0  -10.0  -15.0
+        -4.5   -9.0  -13.5
+        -4.0   -8.0  -12.0
+        -3.5   -7.0  -10.5
+    ] 
+    @test b.dom === a 
 end
