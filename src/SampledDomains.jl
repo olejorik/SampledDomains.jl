00d51ba0461df4877f9d899d92bbcd33edaf5816
Oleg Soloviev <oleg.soloviev@gmail.com>
2023-08-21 17:52:26 +0200

    added iterations and indexing to 2D domains

--- src/SampledDomains.jl
+++ src/SampledDomains.jl
@@ -6,32 +6,46 @@ module SampledDomains
 abstract type AbstractDomain end
 
 import Base.:*
-import Base.size
+import Base: size, length, axes, getindex
+import Base.iterate, Base.IteratorSize, Base.IndexStyle
 
 export CartesianDomain2D, make_centered_domain2D, dualRange, dualDomain
 
-# TODO rewrite as parametric type
-struct CartesianDomain2D<:AbstractDomain
-    xrange::AbstractRange
-    yrange::AbstractRange
-end
+getranges(dom::AbstractDomain) = [getfield(dom, n) for n in fieldnames(typeof(dom))]
 
-getranges(dom::AbstractDomain) = [getfield(dom,n) for n in fieldnames(typeof(dom))]
-
+length(dom::AbstractDomain) = prod(length.(getranges(dom)))
 size(dom::AbstractDomain) = tuple(length.(reverse(getranges(dom)))...) # we need reverse to follow column major rule
 # size(dom::AbstractDomain, d) = length(getfield(dom,d))
 size(dom::AbstractDomain, d) = size(dom)[d]
 
-*(dom::CartesianDomain2D, s::Real) = CartesianDomain2D(dom.xrange * s, dom.yrange*s)
-*( s::Real, dom::CartesianDomain2D) = CartesianDomain2D(dom.xrange * s, dom.yrange*s)
+# TODO rewrite as parametric type
+struct CartesianDomain2D <: AbstractDomain
+    xrange::AbstractRange
+    yrange::AbstractRange
+end
 
+*(dom::CartesianDomain2D, s::Real) = CartesianDomain2D(dom.xrange * s, dom.yrange * s)
+*(s::Real, dom::CartesianDomain2D) = CartesianDomain2D(dom.xrange * s, dom.yrange * s)
+
+function getindex(dom::CartesianDomain2D, I::Vararg{Int,2})
+    return collect(x[i] for (x, i) in zip(reverse(getranges(dom)), I))
+end
+
+Base.IndexStyle(::Type{<:CartesianDomain2D}) = IndexCartesian()
+
+function iterate(dom::CartesianDomain2D, state)
+    return iterate(Iterators.product(reverse(getranges(dom))...), state)
+end
+iterate(dom::CartesianDomain2D) = iterate(Iterators.product(reverse(getranges(dom))...))
+# axes(dom::CartesianDomain2D) = axes(Iterators.product(getranges(dom)...))
+# ndims(dom::CartesianDomain2D) = ndims(Iterators.product(getranges(dom)...))
+IteratorSize(dom::CartesianDomain2D) = IteratorSize(Iterators.product(getranges(dom)...))
 
 function make_centered_domain2D(xlength, ylength, pixelsize)
     xrange = ((1:xlength) .- (1 + xlength) / 2) .* pixelsize
     yrange = ((1:ylength) .- (1 + ylength) / 2) .* pixelsize
-    CartesianDomain2D(xrange, yrange)
+    return CartesianDomain2D(xrange, yrange)
 end
-
 
 """
     dualRange(xrange::AbstractRange, q::Int =1)
@@ -39,21 +53,20 @@ end
 Construct range in Fourier-transform-dual domain with upsampling factor `q`. If sampling in `x` has step size Δx, the dual
 domain is sampled in interval (-q/2Δx, q/2Δx).
 """
-function dualRange(xrange::AbstractRange, q::Int =1)
-    len = length(xrange) *q
-    st = step(xrange) /q
+function dualRange(xrange::AbstractRange, q::Int=1)
+    len = length(xrange) * q
+    st = step(xrange) / q
     return UnitRange(-floor(Int, len / 2), ceil(Int, len / 2) - 1) / (st * len)
-    
 end
 
-function dualDomain(dom::CartesianDomain2D, q::Int = 1)
+function dualDomain(dom::CartesianDomain2D, q::Int=1)
     kxrange = dualRange(dom.xrange, q)
     kyrange = dualRange(dom.yrange, q)
-    return CartesianDomain2D(kxrange,kyrange)
+    return CartesianDomain2D(kxrange, kyrange)
 end
 
 """
-    SampledDomain(vals::Array, dom::AbstractDomain) 
+    SampledDomain(vals::Array, dom::AbstractDomain)
     SampledDomain(f::Function, dom::AbstractDomain)
 
 Contains two fileds: `vals` and `dom` representing sampled values of function `f` on `dom`ain.
@@ -61,9 +74,19 @@ Contains two fileds: `vals` and `dom` representing sampled values of function `f
 struct SampledDomain
     vals::Array
     dom::AbstractDomain
-    SampledDomain(vals::Array, dom::AbstractDomain) = size(vals) != size(dom) ? ErrorException("Different size of the domain and values array") : new(vals, dom)
+    function SampledDomain(vals::Array, dom::AbstractDomain)
+        return if size(vals) != size(dom)
+            ErrorException("Different size of the domain and values array")
+        else
+            new(vals, dom)
+        end
+    end
 end
 
-SampledDomain(f::Function, dom::AbstractDomain) = SampledDomain(map(x->f(x...), collect(Iterators.product(reverse(getranges(dom))...))), dom)
+function SampledDomain(f::Function, dom::AbstractDomain)
+    return SampledDomain(
+        map(x -> f(x...), Iterators.product(reverse(getranges(dom))...)), dom
+    )
+end
 
 end
